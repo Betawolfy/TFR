46e6a8469e7b6d67b47f89b0b2dbc29e2ec9bbf9
betawolfy <contact.betawolfy@gmail.com>
2024-12-23 15:41:06 +0100

    added the getAchievementList commend

---
+++ commands/02_achievements/getAchievementLit.js
@@ -0,0 +1,57 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('get-achievementlist')
+        .setDescription('GET Achievement List'),
+    
+    async execute(interaction) {
+        try {
+            const response = await axios.get('http://localhost:3000/achievements');
+            const achievements = response.data;
+            const achievementEntries = Object.entries(achievements);
+
+            const embed = new EmbedBuilder()
+                .setTitle('[user@tfr-server]~$ get-achievementlist')
+                .setColor('#202020')
+                .setTimestamp()
+                .setFooter({ text: 'Loading achievements...' });
+
+            // Envoyer l'embed initial
+            const message = await interaction.reply({ 
+                embeds: [embed],
+                fetchReply: true 
+            });
+
+            let currentIndex = 0;
+
+            // Ajouter un achievement chaque seconde
+            const interval = setInterval(() => {
+                if (currentIndex >= achievementEntries.length) {
+                    embed.setFooter({ text: 'End-Of-Packet' });
+                    message.edit({ embeds: [embed] });
+                    clearInterval(interval);
+                    return;
+                }
+
+                const [id, achievement] = achievementEntries[currentIndex];
+                embed.addFields({
+                    name: achievement.icon + ' — ' +achievement.numID + '. ' + id,
+                    value: `${achievement.description}, Max value: ${achievement.maxValue}`,
+                });
+
+                message.edit({ embeds: [embed] });
+                currentIndex++;
+            }, 1000);
+
+        } catch (error) {
+            console.error('Erreur lors de la récupération des achievements:', error);
+            await interaction.reply({
+                content: '```bash\n TypeError: An error occurred while fetching achievements.```',
+                ephemeral: true
+            });
+        }
+    },
+};
